9b5dd42b5faa771428bc7b37ff769a23b87a6d68
DkurzA <63888556+DkurzA@users.noreply.github.com>
2023-08-13 03:56:20 +0300

    Update parser1.py
    Reworked scrapper, now with cycles, almost done

--- parser1.py
+++ parser1.py
@@ -4,41 +4,49 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get('https://beta.darkreading.com/edge')
+
+json_file = open('json1.json', 'w+')
+
+
+url = 'https://www.darkreading.com/latest/news'
+response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
 
 today = datetime.today().date()
-site_date = soup.find('div', class_='spotlight-right ml-3')
-parsed_data = datetime.strptime(site_date.text, '%b %d, %Y').date()
 
+one_article = soup.find('div', class_='topic-content-article')  #контейнер с новостью
+article_date = one_article.find('div', class_='arcile-date').text
+test_date = datetime.strptime("2023-08-11", "%Y-%m-%d").date()
+news_count = 0
 
-news_title = soup.find('div', class_='spotlight-left').find('a').text
-today_link = soup.find('div', class_='spotlight-left').find('a').get('href')
+# print(test_date)
 
-news_response = requests.get(today_link)
-soup = BeautifulSoup(news_response.text, 'lxml')
+while True:
+  parsed_date = datetime.strptime(article_date, '%B %d, %Y').date()  #дата в контейнере, приведенная к нужному формату
+  if parsed_date == test_date:
+    news_title = one_article.find(class_='article-title').text  #заголовок новости
+    today_link = one_article.find('a').get('href')  #ссылка на саму новость
+    news_response = requests.get(today_link)  #реквест полученной ссылки
+    news_soup = BeautifulSoup(news_response.text, 'lxml') #суп полученной ссылки
+    paragraph = news_soup.find_all('p')
+    p_list = []
+    for i in paragraph:
+        p_list.append(i.text)
+    news_body = ". ".join(p_list) #полный текст статьи
 
-paragraph = soup.find_all('p')
-p_list = []
-for i in paragraph:
-    p_list.append(i.text)
-news_body = ". ".join(p_list)
-# print(news_body)
+    data = {'date': article_date,  #запись в словарь даты
+            'link': today_link, #запись в словарь ссылки
+            'title': news_title,  #запись в словарь заголовка
+            'body': news_body}  #запись в словарь текста статьи
 
+    json.dump(data, json_file)
 
-data = {'date': site_date.text,
-        'link': today_link,
-        'title': news_title,
-        'body': news_body}
+    news_count += 1
 
-with open('json1.json', 'w') as file:
-    json.dump(data, file)
+    one_article = one_article.find_next('div', class_='topic-content-article')
+    article_date = one_article.find('div', class_='arcile-date').text
 
-
-
-# print(paragraph.text)
-# print(site_date)
-# if parsed_data == today:
-#     print(url)
-# else:
-#     print(site_date)
+  else:
+    json_file.close
+    print(f'За сегодняшний день на сйте: {url} найдено {news_count} новостей')
+    break
